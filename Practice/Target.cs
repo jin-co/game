6b4e2d08f861d71846a8fb2e2f27fb96b7747fb8
jin <lilijk@hanmail.net>
2021-09-20 11:40:59 +0900

    chore: update

---
+++ Practice/Target.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Practice
+{
+    class Target
+    {
+        public Target() {}
+
+        private const int TARGET_RADIUS = 45;
+
+        public Vector2 Position { get; set; }
+       
+        public int getTargetRadius()
+        {
+            return TARGET_RADIUS;
+        }
+    }
+}
