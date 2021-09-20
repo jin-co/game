8f148c831bffb757bee6a6c84015d8c5fc7f0186
jin <lilijk@hanmail.net>
2021-09-20 12:26:33 +0900

    chore: update

--- Practice/Game1.cs
+++ Practice/Game1.cs
@@ -19,10 +19,16 @@ namespace Practice
         Target target = new Target();
 
         MouseState mState;
+        bool mReleased = true;
+
+        KeyboardState kState;
 
         int score = 0;
 
         double timer = 10;
+
+        // game start
+        bool gameStarted = false;
         
         public Game1()
         {
@@ -54,6 +60,12 @@ namespace Practice
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // start game
+            if (kState.IsKeyDown(Keys.Space))
+            {
+                gameStarted = true;
+            }
+
             // timer
             if (timer > 0) timer -= gameTime.ElapsedGameTime.TotalSeconds;
             if (timer < 0) timer = 0; 
@@ -63,44 +75,78 @@ namespace Practice
 
             // score
             float mouseTargetDist = Vector2.Distance(target.Position, mState.Position.ToVector2());
-            
+            if (mState.LeftButton == ButtonState.Pressed && timer > 0)
+            {
+                if (mouseTargetDist < 50 && mReleased) 
+                {
+                    score++;
 
+                    // target
+                    Random random = new Random();
+                    target.Position = new Vector2(
+                        random.Next(target.getTargetRadius(), _graphics.PreferredBackBufferWidth), 
+                        random.Next(target.getTargetRadius(), _graphics.PreferredBackBufferHeight));
+                }
+                mReleased = false;
+            }
 
-
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            target.Position = new Vector2(300, 300);
-            
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
+            // background
             _spriteBatch.Draw(skySprite, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(targetSprite, 
-                new Vector2(
-                    target.Position.X - target.getTargetRadius(), 
-                    target.Position.Y - target.getTargetRadius()
-                    ), 
-                Color.White);
-            _spriteBatch.Draw(crosshairsSprite, 
-                new Vector2(mState.X - 24, mState.Y - 24), 
-                Color.Red);
 
-            // score
-            _spriteBatch.DrawString(gameFont,
-                "Score: " + score.ToString(),
-                new Vector2(10, 10),
-                Color.White);
-            // timer
-            _spriteBatch.DrawString(gameFont,
-                "timer: " + Math.Floor(timer).ToString(),
-                new Vector2(10, 50),
-                Color.White);
+            if (gameStarted)
+            {
+                // target
+                if (timer > 0)
+                {
+                    _spriteBatch.Draw(targetSprite,
+                        new Vector2(
+                            target.Position.X - target.getTargetRadius(),
+                            target.Position.Y - target.getTargetRadius()
+                            ),
+                        Color.White);
+                }
+
+                _spriteBatch.Draw(crosshairsSprite,
+                    new Vector2(mState.X - 24, mState.Y - 24),
+                    Color.Red);
+
+                // score
+                _spriteBatch.DrawString(gameFont,
+                    "Score: " + score.ToString(),
+                    new Vector2(10, 10),
+                    Color.White);
+
+                // timer
+                _spriteBatch.DrawString(gameFont,
+                    "timer: " + Math.Floor(timer).ToString(),
+                    new Vector2(10, 50),
+                    Color.White);
+            }
+
+            else
+            {
+                _spriteBatch.DrawString(gameFont,
+                    "Start", new Vector2(
+                        _graphics.PreferredBackBufferWidth / 2,
+                        _graphics.PreferredBackBufferHeight / 2),
+                    Color.Black
+                    );
+            }
 
             // test
             _spriteBatch.DrawString(gameFont, "mState" + mState.Position.ToVector2().ToString(), new Vector2(10, 150), Color.White);
