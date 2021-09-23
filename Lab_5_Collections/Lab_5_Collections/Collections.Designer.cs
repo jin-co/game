9b2401b722dfec8a6399a48be97ccfd24d45e5e9
jin <lilijk@hanmail.net>
2021-09-23 21:15:28 +0900

    chore: update

---
+++ Lab_5_Collections/Lab_5_Collections/Collections.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Lab_5_Collections
+{
+    partial class Collections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArrayList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArrayList
+            // 
+            this.btnArrayList.Location = new System.Drawing.Point(38, 50);
+            this.btnArrayList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnArrayList.Name = "btnArrayList";
+            this.btnArrayList.Size = new System.Drawing.Size(112, 32);
+            this.btnArrayList.TabIndex = 0;
+            this.btnArrayList.Text = "ArrayList";
+            this.btnArrayList.UseVisualStyleBackColor = true;
+            this.btnArrayList.Click += new System.EventHandler(this.btnArrayList_Click);
+            // 
+            // Collections
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 623);
+            this.Controls.Add(this.btnArrayList);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Collections";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArrayList;
+    }
+}
+
---
+++ Lab_5_Collections/Lab_5_Collections/Collections.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5_Collections
+{
+    public partial class Collections : Form
+    {
+        public Collections()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArrayList_Click(object sender, EventArgs e)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("String value");
+            arrayList.Add(100);
+            arrayList.Add(3.14f);
+            Button button = new Button();
+            button.Text = "This is a button";
+            arrayList.Add(button);
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                if (arrayList[i] is Button)
+                {
+                    Debug.WriteLine(((Button)arrayList[i]).Text);
+                }
+                Debug.WriteLine(arrayList[i]);
+            }
+
+        }
+    }
+}
