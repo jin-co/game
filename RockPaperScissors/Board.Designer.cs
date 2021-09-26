e065348d43c3a2a4639809680a230213fad86acb
jin-co <lilijk@hanmail.net>
2021-09-26 11:54:36 +0900

    chore: update

---
+++ RockPaperScissors/Board.Designer.cs
@@ -0,0 +1,373 @@
+﻿
+namespace RockPaperScissors
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.gamePic9 = new System.Windows.Forms.PictureBox();
+            this.gamePic8 = new System.Windows.Forms.PictureBox();
+            this.gamePic7 = new System.Windows.Forms.PictureBox();
+            this.gamePic6 = new System.Windows.Forms.PictureBox();
+            this.gamePic5 = new System.Windows.Forms.PictureBox();
+            this.gamePic4 = new System.Windows.Forms.PictureBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.gamePic3 = new System.Windows.Forms.PictureBox();
+            this.gamePic2 = new System.Windows.Forms.PictureBox();
+            this.gamePic1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtScore1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtScore2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ptbReferee = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnChangeReferee = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbReferee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(547, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Player 2";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.gamePic9);
+            this.groupBox.Controls.Add(this.gamePic8);
+            this.groupBox.Controls.Add(this.gamePic7);
+            this.groupBox.Controls.Add(this.gamePic6);
+            this.groupBox.Controls.Add(this.gamePic5);
+            this.groupBox.Controls.Add(this.gamePic4);
+            this.groupBox.Controls.Add(this.btnNewGame);
+            this.groupBox.Controls.Add(this.gamePic3);
+            this.groupBox.Controls.Add(this.gamePic2);
+            this.groupBox.Controls.Add(this.gamePic1);
+            this.groupBox.Location = new System.Drawing.Point(202, 58);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(342, 409);
+            this.groupBox.TabIndex = 4;
+            this.groupBox.TabStop = false;
+            // 
+            // gamePic9
+            // 
+            this.gamePic9.BackColor = System.Drawing.Color.White;
+            this.gamePic9.Location = new System.Drawing.Point(228, 252);
+            this.gamePic9.Name = "gamePic9";
+            this.gamePic9.Size = new System.Drawing.Size(105, 105);
+            this.gamePic9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic9.TabIndex = 11;
+            this.gamePic9.TabStop = false;
+            this.gamePic9.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic8
+            // 
+            this.gamePic8.BackColor = System.Drawing.Color.White;
+            this.gamePic8.Location = new System.Drawing.Point(117, 252);
+            this.gamePic8.Name = "gamePic8";
+            this.gamePic8.Size = new System.Drawing.Size(105, 105);
+            this.gamePic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic8.TabIndex = 10;
+            this.gamePic8.TabStop = false;
+            this.gamePic8.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic7
+            // 
+            this.gamePic7.BackColor = System.Drawing.Color.White;
+            this.gamePic7.Location = new System.Drawing.Point(6, 252);
+            this.gamePic7.Name = "gamePic7";
+            this.gamePic7.Size = new System.Drawing.Size(105, 105);
+            this.gamePic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic7.TabIndex = 9;
+            this.gamePic7.TabStop = false;
+            this.gamePic7.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic6
+            // 
+            this.gamePic6.BackColor = System.Drawing.Color.White;
+            this.gamePic6.Location = new System.Drawing.Point(228, 141);
+            this.gamePic6.Name = "gamePic6";
+            this.gamePic6.Size = new System.Drawing.Size(105, 105);
+            this.gamePic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic6.TabIndex = 8;
+            this.gamePic6.TabStop = false;
+            this.gamePic6.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic5
+            // 
+            this.gamePic5.BackColor = System.Drawing.Color.White;
+            this.gamePic5.Location = new System.Drawing.Point(117, 141);
+            this.gamePic5.Name = "gamePic5";
+            this.gamePic5.Size = new System.Drawing.Size(105, 105);
+            this.gamePic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic5.TabIndex = 7;
+            this.gamePic5.TabStop = false;
+            this.gamePic5.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic4
+            // 
+            this.gamePic4.BackColor = System.Drawing.Color.White;
+            this.gamePic4.Location = new System.Drawing.Point(6, 141);
+            this.gamePic4.Name = "gamePic4";
+            this.gamePic4.Size = new System.Drawing.Size(105, 105);
+            this.gamePic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic4.TabIndex = 6;
+            this.gamePic4.TabStop = false;
+            this.gamePic4.Click += new System.EventHandler(this.box_click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(6, 374);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(327, 29);
+            this.btnNewGame.TabIndex = 5;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // gamePic3
+            // 
+            this.gamePic3.BackColor = System.Drawing.Color.White;
+            this.gamePic3.Location = new System.Drawing.Point(228, 30);
+            this.gamePic3.Name = "gamePic3";
+            this.gamePic3.Size = new System.Drawing.Size(105, 105);
+            this.gamePic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic3.TabIndex = 2;
+            this.gamePic3.TabStop = false;
+            this.gamePic3.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic2
+            // 
+            this.gamePic2.BackColor = System.Drawing.Color.White;
+            this.gamePic2.Location = new System.Drawing.Point(117, 30);
+            this.gamePic2.Name = "gamePic2";
+            this.gamePic2.Size = new System.Drawing.Size(105, 105);
+            this.gamePic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic2.TabIndex = 1;
+            this.gamePic2.TabStop = false;
+            this.gamePic2.Click += new System.EventHandler(this.box_click);
+            // 
+            // gamePic1
+            // 
+            this.gamePic1.BackColor = System.Drawing.Color.White;
+            this.gamePic1.Location = new System.Drawing.Point(6, 30);
+            this.gamePic1.Name = "gamePic1";
+            this.gamePic1.Size = new System.Drawing.Size(105, 105);
+            this.gamePic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gamePic1.TabIndex = 0;
+            this.gamePic1.TabStop = false;
+            this.gamePic1.Click += new System.EventHandler(this.box_click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Score: ";
+            // 
+            // txtScore1
+            // 
+            this.txtScore1.Location = new System.Drawing.Point(93, 230);
+            this.txtScore1.Name = "txtScore1";
+            this.txtScore1.Size = new System.Drawing.Size(103, 24);
+            this.txtScore1.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RockPaperScissors.Properties.Resources.p15_208_primage_2;
+            this.pictureBox2.Location = new System.Drawing.Point(550, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 166);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RockPaperScissors.Properties.Resources.maxresdefault;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtScore2
+            // 
+            this.txtScore2.Location = new System.Drawing.Point(600, 230);
+            this.txtScore2.Name = "txtScore2";
+            this.txtScore2.Size = new System.Drawing.Size(103, 24);
+            this.txtScore2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(548, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Score: ";
+            // 
+            // ptbReferee
+            // 
+            this.ptbReferee.Location = new System.Drawing.Point(43, 335);
+            this.ptbReferee.Name = "ptbReferee";
+            this.ptbReferee.Size = new System.Drawing.Size(152, 126);
+            this.ptbReferee.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbReferee.TabIndex = 9;
+            this.ptbReferee.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Referee";
+            // 
+            // btnChangeReferee
+            // 
+            this.btnChangeReferee.Location = new System.Drawing.Point(120, 306);
+            this.btnChangeReferee.Name = "btnChangeReferee";
+            this.btnChangeReferee.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeReferee.TabIndex = 11;
+            this.btnChangeReferee.Text = "Change";
+            this.btnChangeReferee.UseVisualStyleBackColor = true;
+            this.btnChangeReferee.Click += new System.EventHandler(this.btnChangeReferee_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(550, 275);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 29);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(750, 531);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnChangeReferee);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ptbReferee);
+            this.Controls.Add(this.txtScore2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtScore1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Board";
+            this.Text = "Game Board";
+            this.groupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbReferee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.PictureBox gamePic9;
+        private System.Windows.Forms.PictureBox gamePic8;
+        private System.Windows.Forms.PictureBox gamePic7;
+        private System.Windows.Forms.PictureBox gamePic6;
+        private System.Windows.Forms.PictureBox gamePic5;
+        private System.Windows.Forms.PictureBox gamePic4;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.PictureBox gamePic3;
+        private System.Windows.Forms.PictureBox gamePic2;
+        private System.Windows.Forms.PictureBox gamePic1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtScore1;
+        private System.Windows.Forms.TextBox txtScore2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox ptbReferee;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnChangeReferee;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ RockPaperScissors/Board.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Board : Form
+    {
+        // for using sound
+        private SoundPlayer soundPlayer = null;
+
+        // Define possible turn
+        private enum PlayerTurn
+        {
+            Player1,
+            Player2
+        }
+
+        // Starting turn
+        private PlayerTurn currentTurn = PlayerTurn.Player1;
+
+        // Game status
+        private const string PLAYER1_WINS = "Player 1 Win";
+        private const string PLAYER2_WINS = "Player 2 Win";
+        private const string TIE = "Tie";
+
+        // Images
+        private List<Image> images = new List<Image>();
+
+        // Images for referee
+        private List<Image> imagesReferee = new List<Image> {
+            Resources.referee1,
+            Resources.referee2,
+            Resources.referee3,
+            Resources.referee4,
+            Resources.referee5,
+            Resources.referee6
+            }; 
+
+        // Players pick
+        private Image player1Pick;
+        private Image player2Pick;
+
+        // Scores
+        int score1 = 0;
+        int score2 = 0;
+
+        public Board()
+        {
+            InitializeComponent();
+            // Load game images
+            LoadGameImages();
+        }
+
+        private void box_click(object sender, EventArgs e)
+        {
+            // enable unified click event
+            PictureBox picClicked = sender as PictureBox;
+            groupBox.Text = "";
+            Random random = new Random();
+
+            int index = random.Next(0, 200) % 3; // 0, 1, 2
+
+            // for easy debugging
+            Debug.WriteLine("generated index = " + index);
+
+            if (currentTurn == PlayerTurn.Player1)
+            {
+                player1Pick = images[index];
+            }
+            else
+            {
+                player2Pick = images[index];
+                ShowWinner();
+            }
+
+            // display image
+            picClicked.Image = images[index];
+
+            // allocate the turns
+            currentTurn = currentTurn == PlayerTurn.Player1 ? PlayerTurn.Player2 : PlayerTurn.Player1;
+
+            // for sound to be working instantiate then **play
+            soundPlayer = new SoundPlayer(Resources.blip);
+            soundPlayer.Play();
+        }
+
+        private void ShowWinner()
+        {
+            // just for clarity
+            int rock = 0;
+            int paper = 1;
+            int scissors = 2;
+
+            // player 1
+            if (player1Pick == images[rock])
+            {
+                if (player2Pick == images[rock]) groupBox.Text = TIE;
+                if (player2Pick == images[paper])
+                {
+                    groupBox.Text = PLAYER2_WINS;
+                    score2++;
+                }
+                if (player2Pick == images[scissors])
+                {
+                    groupBox.Text = PLAYER1_WINS;
+                    score1++;
+                }
+            }
+            if (player1Pick == images[paper])
+            {
+                if (player2Pick == images[paper]) groupBox.Text = TIE;
+                if (player2Pick == images[scissors])
+                {
+                    groupBox.Text = PLAYER2_WINS;
+                    score2++;
+                }
+                if (player2Pick == images[rock])
+                {
+                    groupBox.Text = PLAYER1_WINS;
+                    score1++;
+                }
+            }
+            if (player1Pick == images[scissors])
+            {
+                if (player2Pick == images[scissors]) groupBox.Text = TIE;
+                if (player2Pick == images[rock])
+                {
+                    groupBox.Text = PLAYER2_WINS;
+                    score2++;
+                }
+                if (player2Pick == images[paper])
+                {
+                    groupBox.Text = PLAYER1_WINS;
+                    score1++;
+                }
+            }
+
+            txtScore1.Text = score1.ToString();
+            txtScore2.Text = score2.ToString();
+        }
+
+        private void LoadGameImages()
+        {
+            images.Add(Resources.rock);
+            images.Add(Resources.paper);
+            images.Add(Resources.scissors);
+
+            Random random = new Random();
+            ptbReferee.Image = imagesReferee[random.Next(7)];
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            PictureBox[] gameImges =
+            {
+                gamePic1,
+                gamePic2,
+                gamePic3,
+                gamePic4,
+                gamePic5,
+                gamePic6,
+                gamePic7,
+                gamePic8,
+                gamePic9
+            };
+            foreach (var j in gameImges)
+            {
+                j.Image = null;
+            }
+            //gamePic1.Image = null;
+            //gamePic2.Image = null;
+            //gamePic3.Image = null;
+            //gamePic4.Image = null;
+            //gamePic5.Image = null;
+            //gamePic6.Image = null;
+            //gamePic7.Image = null;
+            //gamePic8.Image = null;
+            //gamePic9.Image = null;
+
+            player1Pick = null;
+            player2Pick = null;
+
+            currentTurn = PlayerTurn.Player1;
+
+            score1 = 0;
+            score2 = 0;
+            txtScore1.Text = score1.ToString();
+            txtScore2.Text = score2.ToString();
+        }
+
+        private void btnChangeReferee_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            ptbReferee.Image = imagesReferee[random.Next(6)];
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
