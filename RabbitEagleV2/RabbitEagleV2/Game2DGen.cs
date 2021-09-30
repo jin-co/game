5cf69b3b66169377e62b308f654127436f4693ff
jin-co <lilijk@hanmail.net>
2021-09-30 21:47:08 +0900

    feat: complete super class

--- RabbitEagleV2/RabbitEagleV2/Game2DGen.cs
+++ RabbitEagleV2/RabbitEagleV2/Game2DGen.cs
@@ -8,14 +8,15 @@ using System.Windows.Forms;
 
 namespace RabbitEagleV2
 {
+    // player's super class
     // generating game component
-    class Game2DGen
+    abstract class Game2DGen
     {
         protected Control parent;
         protected int width, height;
-        protected Color backColor;
+        protected Color backColor = default;
 
-        private Color defaultColor;
+        private Color defaultColor = Color.White;
         public int gapX, gapY;
         public int startX, startY;
 
@@ -33,6 +34,8 @@ namespace RabbitEagleV2
             int x = startX, y = startY;
             int number = 0;
             PictureBox[,] pictureBoxes = new PictureBox[rows, cols];
+
+            // generates rows and columns of the picture boxes and set properties
             for (int row = 0; row < rows; row++)
             {
                 for (int col = 0; col < cols; col++)
@@ -42,9 +45,24 @@ namespace RabbitEagleV2
                     p.Top = y;
                     p.Width = width;
                     p.Height = height;
-                }
-            }
+                    // setting the size of generated picture box
+                    p.SizeMode = PictureBoxSizeMode.StretchImage;
+                    p.Name = "pictureBox" + number;
+                    p.BackColor = backColor;
 
+                    pictureBoxes[row, col] = p;
+
+                    x += width + gapX;
+                    number++;
+                }
+                x = startX;
+                y += height + gapX;
+            }
+            return pictureBoxes;
         }
+
+        public abstract void LoadContent();
+        public abstract void Update();
+        public abstract void UnloadContent();
     }
 }
