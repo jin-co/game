8020eaf015aa9784271fdbc76b52a1010a3abe2b
jin-co <lilijk@hanmail.net>
2021-10-06 01:51:19 +0900

    chore: update

---
+++ PuzzleGame/PuzzleGame/Puzzle.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace PuzzleGame
+{
+    partial class Puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDesign = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDesign
+            // 
+            this.btnDesign.Location = new System.Drawing.Point(142, 82);
+            this.btnDesign.Name = "btnDesign";
+            this.btnDesign.Size = new System.Drawing.Size(166, 47);
+            this.btnDesign.TabIndex = 0;
+            this.btnDesign.Text = "Design";
+            this.btnDesign.UseVisualStyleBackColor = true;
+            this.btnDesign.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(142, 147);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(166, 47);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(142, 213);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(166, 47);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 432);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnDesign);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Puzzle";
+            this.Text = "Puzzle";
+            this.Load += new System.EventHandler(this.Puzzle_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDesign;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PuzzleGame/PuzzleGame/Puzzle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleGame
+{
+    public partial class Puzzle : Form
+    {
+        public Puzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void Puzzle_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Design design = new Design();
+            design.Show();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Play play = new Play();
+            play.Show();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
