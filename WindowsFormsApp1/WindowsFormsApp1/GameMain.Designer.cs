33995aff978c15a5b9ef8335d0f1f97c5cf99357
jin-co <lilijk@hanmail.net>
2021-10-14 21:24:23 +0900

    chore: update

---
+++ WindowsFormsApp1/WindowsFormsApp1/GameMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class GameMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.txtColumns = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.grbGrid = new System.Windows.Forms.GroupBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rows";
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(95, 34);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(100, 24);
+            this.txtRows.TabIndex = 1;
+            // 
+            // txtColumns
+            // 
+            this.txtColumns.Location = new System.Drawing.Point(290, 34);
+            this.txtColumns.Name = "txtColumns";
+            this.txtColumns.Size = new System.Drawing.Size(100, 24);
+            this.txtColumns.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Columns";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(496, 27);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(114, 38);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(648, 27);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(114, 38);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // grbGrid
+            // 
+            this.grbGrid.Location = new System.Drawing.Point(45, 83);
+            this.grbGrid.Name = "grbGrid";
+            this.grbGrid.Size = new System.Drawing.Size(717, 511);
+            this.grbGrid.TabIndex = 6;
+            this.grbGrid.TabStop = false;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(789, 41);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(389, 24);
+            this.txtMessage.TabIndex = 7;
+            // 
+            // GameMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 623);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.grbGrid);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtColumns);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GameMain";
+            this.Text = "Image loader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.TextBox txtColumns;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.GroupBox grbGrid;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/GameMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class GameMain : Form
+    {
+        private int numberOfImages = 0;
+        public GameMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int row;
+            if (int.TryParse(txtRows.Text, out row))
+            {
+                txtMessage.Text += "not integer";
+            }
+
+            try
+            {
+                int.Parse(txtRows.Text);
+                int.Parse(txtColumns.Text);
+            }
+            catch (Exception error)
+            {                
+                txtMessage.Text = error.Message;
+                txtRows.Text = "";
+                txtColumns.Text = "";
+            }
+
+            int rows = int.Parse(txtRows.Text);
+            int cols = int.Parse(txtColumns.Text);
+            if (rows * cols > numberOfImages)
+            {
+                txtMessage.Text = "image number error";
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
