41f08ddb4719b5c5eaa16d2fdf9a926dd98f95be
jin-co <lilijk@hanmail.net>
2021-10-18 20:39:49 +0900

    chore: update

---
+++ PicturePuzzle/PicturePuzzle/GameEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicturePuzzle
+{
+    class GameEventHandler : PuzzleData
+    {
+        private PictureBox fromBox;
+        private PictureBox toBox;
+    }
+}
