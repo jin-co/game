b2bc1d1d2dd6dfa7522fde2ba60c83cebd72c2ee
jin-co <lilijk@hanmail.net>
2021-10-23 07:40:41 +0900

    feat: complete first, last image load

---
+++ KBMIdterm/LoadImage/LoadImage/MainGame.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace LoadImage
+{
+    partial class MainGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.btnAutomatic = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.ptbSlider = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(23, 485);
+            this.btnLoadImage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(477, 52);
+            this.btnLoadImage.TabIndex = 4;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnManual);
+            this.groupBox1.Controls.Add(this.btnAutomatic);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(477, 105);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View style";
+            // 
+            // btnManual
+            // 
+            this.btnManual.Location = new System.Drawing.Point(297, 37);
+            this.btnManual.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(149, 52);
+            this.btnManual.TabIndex = 8;
+            this.btnManual.Text = "Manual";
+            this.btnManual.UseVisualStyleBackColor = true;
+            this.btnManual.Click += new System.EventHandler(this.btnManual_Click);
+            // 
+            // btnAutomatic
+            // 
+            this.btnAutomatic.Location = new System.Drawing.Point(42, 37);
+            this.btnAutomatic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAutomatic.Name = "btnAutomatic";
+            this.btnAutomatic.Size = new System.Drawing.Size(149, 52);
+            this.btnAutomatic.TabIndex = 7;
+            this.btnAutomatic.Text = "Automatic";
+            this.btnAutomatic.UseVisualStyleBackColor = true;
+            this.btnAutomatic.Click += new System.EventHandler(this.btnAutomatic_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(65, 236);
+            this.btnFirst.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(86, 52);
+            this.btnFirst.TabIndex = 9;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(171, 236);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(86, 52);
+            this.btnPrevious.TabIndex = 10;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(277, 236);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(86, 52);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(383, 236);
+            this.btnLast.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(86, 52);
+            this.btnLast.TabIndex = 12;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(171, 347);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(86, 52);
+            this.btnPlay.TabIndex = 13;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(277, 347);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(86, 52);
+            this.btnStop.TabIndex = 14;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // ptbSlider
+            // 
+            this.ptbSlider.Location = new System.Drawing.Point(578, 99);
+            this.ptbSlider.Name = "ptbSlider";
+            this.ptbSlider.Size = new System.Drawing.Size(427, 371);
+            this.ptbSlider.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbSlider.TabIndex = 15;
+            this.ptbSlider.TabStop = false;
+            // 
+            // MainGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 570);
+            this.Controls.Add(this.ptbSlider);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnLoadImage);
+            this.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainGame";
+            this.Text = "Another way to load image into memory";
+            this.Load += new System.EventHandler(this.MainGame_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSlider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Button btnAutomatic;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox ptbSlider;
+    }
+}
+
---
+++ KBMIdterm/LoadImage/LoadImage/MainGame.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadImage
+{
+    public partial class MainGame : Form
+    {
+        
+
+        
+        string path = Path.GetFullPath(@"../../..");
+        public MainGame()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGame_Load(object sender, EventArgs e)
+        {
+            btnFirst.Visible = true;
+            btnPrevious.Visible = true;
+            btnNext.Visible = true;
+            btnLast.Visible = true;
+            btnPlay.Visible = false;
+            btnStop.Visible = false;
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            Image[] images = new Image[5];
+
+            // load images
+            int max = 5;
+
+            for (int i = 0; i < max; i++)
+            {
+                string fname = "image" + i + ".jpg"; // fname = $"p{i}.jpg";
+                string path = Path.GetFullPath(@"../../..");
+                images[i] = Image.FromFile($@"{path}/images/{fname}");
+                MessageBox.Show(i + "added");
+            }
+        }
+
+        private void btnAutomatic_Click(object sender, EventArgs e)
+        {
+            btnFirst.Visible = false;
+            btnPrevious.Visible = false;
+            btnNext.Visible = false;
+            btnLast.Visible = false;
+            btnPlay.Visible = true;
+            btnStop.Visible = true;
+        }
+
+        private void btnManual_Click(object sender, EventArgs e)
+        {
+            btnFirst.Visible = true;
+            btnPrevious.Visible = true;
+            btnNext.Visible = true;
+            btnLast.Visible = true;
+            btnPlay.Visible = false;
+            btnStop.Visible = false;
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            string fname = "image" + 1 + ".jpg"; // fname = $"p{i}.jpg";
+            Image image = Image.FromFile($@"{path}/images/{fname}");
+            ptbSlider.Image = image;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            string fname = "image" + 5 + ".jpg"; // fname = $"p{i}.jpg";
+            Image image = Image.FromFile($@"{path}/images/{fname}");
+            ptbSlider.Image = image;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
