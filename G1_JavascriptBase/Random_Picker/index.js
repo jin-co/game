aa1d057dd6abeee6bfecab2678117e9846f25da7
jin-co <lilijk@hanmail.net>
2021-11-05 11:30:35 +0900

    chore: complete rotation effect

--- G1_JavascriptBase/Random_Picker/index.js
+++ G1_JavascriptBase/Random_Picker/index.js
@@ -5,32 +5,44 @@ let idx
 
 moves.forEach((move, index) => {
     move.addEventListener('click', () => {
-        console.log('c')
-        // two
-        pages[0].style.transform = 'translate(0%, 100%)'
-        pages[1].style.transform = 'translate(0%, -100%)'
-        pages[2].style.transform = 'translate(100%, -100%)'
-        pages[3].style.transform = 'translate(100%, 0%)'
+        console.log(index)
+        shuffleCard(index)
+        
         // move.style.transform = 'translate()'
     })
 });
 
-function test() {
-    // three
-    pages[0].style.transform = 'translate(100%, 100%)'
-    pages[1].style.transform = 'translate(0%, 0%)'
-    pages[2].style.transform = 'translate(100%, -100%)'
-    pages[3].style.transform = 'translate(100%, 0%)'
-    
-    // four
-    pages[0].style.transform = 'translate(0%, 100%)'
-    pages[1].style.transform = 'translate(0%, -100%)'
-    pages[2].style.transform = 'translate(0%, 0%)'
-    pages[3].style.transform = 'translate(100%, 0%)'
-    
-    // one
-    pages[0].style.transform = 'translate(0%, 0%)'
-    pages[1].style.transform = 'translate(100%, -100%)'
-    pages[2].style.transform = 'translate(100%, 0%)'
-    pages[3].style.transform = 'translate(0%, 0%)'
+function shuffleCard(index) {
+    switch (index) {
+        case 0:
+            // two
+            pages[0].style.transform = 'translate(0%, 100%)'
+            pages[1].style.transform = 'translate(0%, -100%)'
+            pages[2].style.transform = 'translate(100%, -100%)'
+            pages[3].style.transform = 'translate(100%, 0%)'
+            break;
+        case 1:
+            // three
+            pages[0].style.transform = 'translate(100%, 100%)'
+            pages[1].style.transform = 'translate(0%, 0%)'
+            pages[2].style.transform = 'translate(0%, -100%)'
+            pages[3].style.transform = 'translate(100%, -100%)'
+            break;
+        case 2:
+            // four
+            pages[0].style.transform = 'translate(100%, 0%)'
+            pages[1].style.transform = 'translate(100%, 0%)'
+            pages[2].style.transform = 'translate(0%, 0%)'
+            pages[3].style.transform = 'translate(0%, -100%)'
+            break;
+        case 3:
+            // one
+            pages[0].style.transform = 'translate(0%, 0%)'
+            pages[1].style.transform = 'translate(100%, -100%)'
+            pages[2].style.transform = 'translate(100%, 0%)'
+            pages[3].style.transform = 'translate(0%, 0%)'
+            break;    
+        default:
+            break;
+    }
 }
