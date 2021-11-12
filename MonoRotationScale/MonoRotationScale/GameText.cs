fef681fe3aaf8b9a5f228ef83eb746e3eb489220
jin-co <lilijk@hanmail.net>
2021-11-12 22:20:49 +0900

    launch: initialize rotation app

---
+++ MonoRotationScale/MonoRotationScale/GameText.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoRotationScale
+{
+    class GameText
+    {
+        private Texture2D entiry;
+        private Vector2 origin;
+        private float scale = 0.5f;
+        private float scaleChange = 0.01f;
+        private Rectangle sourceRectangle;
+        public float rotation;
+        private float rotationChange = 0.03f;
+    }
+}
---
+++ MonoRotationScale/MonoRotationScale/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoRotationScale
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
