4dd56bb90955b29f4d69ea9fc9f6d4a772e8044c
jin-co <lilijk@hanmail.net>
2021-11-16 21:49:56 +0900

    chore: finish recreating board from file

--- KBaekQGame/KBaekQGame/GamePlay.cs
+++ KBaekQGame/KBaekQGame/GamePlay.cs
@@ -17,20 +17,36 @@ namespace KBaekQGame
         public GamePlay()
         {
             InitializeComponent();
+            imageList1.Images.Add(Resources.none);
+            imageList1.Images.Add(Resources.wall);
+            imageList1.Images.Add(Resources.reddoor);
+            imageList1.Images.Add(Resources.greendoor);
+            imageList1.Images.Add(Resources.redbox);
+            imageList1.Images.Add(Resources.greenbox);
         }
 
         private void GamePlay_Load(object sender, EventArgs e)
         {
-            game = new Game(spcPlayBoard, imageList1);
+            new Game(spcPlayBoard, imageList1);
         }
 
         private void loadGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string loadString = FileHandler.LoadFile();                        
+            //string loadString = FileHandler.LoadFile();
+            char[] loadString = FileHandler.LoadFile().ToCharArray();
             Game.Rows = int.Parse(loadString[0].ToString());
             Game.Cols = int.Parse(loadString[1].ToString());
             Game.Cubes = new PictureBox[Game.Rows, Game.Cols];
             GameGenerator.GenerateGame();
+            for (int i = 2, j = 3, k = 4; k < loadString.Length; i += 3, j += 3, k +=3)
+            {
+                if (int.Parse(loadString[k].ToString()) != 0)
+                {
+                    Game.Cubes[int.Parse(loadString[i].ToString()), 
+                        int.Parse(loadString[j].ToString())].Image = 
+                        Game.ImageList.Images[int.Parse(loadString[k].ToString())];
+                }
+            }
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
