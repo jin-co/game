76af4c9ab8a5a3f0d082fdfaabb8a99b13b4a6e9
jin-co <lilijk@hanmail.net>
2021-11-28 14:44:57 +0900

    chore: update

---
+++ MonoMenu/MonoMenu/GameMain.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoMenu
+{
+    public class GameMain : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D backgroundSprite;
+        Texture2D aboutSprite;
+        Texture2D aboutBtnSprite;
+        Texture2D helpSprite;
+        Texture2D helpBtnSprite;
+        Texture2D menuSprite;
+        Texture2D menuBtnSprite;
+
+        SpriteFont gameFont;
+
+        private int screenWidth = 1600;
+        private int screenHeight = 900;
+
+        public GameMain()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenWidth;
+            _graphics.PreferredBackBufferHeight = screenHeight;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            backgroundSprite = Content.Load<Texture2D>("background");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(50, 50), Color.White);
+
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
