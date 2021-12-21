b0d648aa180e43f724e9689f401da7a91ea9b905
jin-co <lilijk@hanmail.net>
2021-12-21 19:51:11 +0900

    chore

---
+++ MonoKingdom/MonoKingdom/GameMain.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoKingdom
+{
+    public class GameMain : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private int screenWidth = 1900;
+        private int screenHeight = 1000;
+
+        public GameMain()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenWidth;
+            _graphics.PreferredBackBufferHeight = screenHeight;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            GameSprites.Content = Content;
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(GameSprites.LoadSprite("Sprites/maps/Map_tile_01"), new Vector2(0, 0), Color.White);
+
+
+
+
+
+
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoKingdom/MonoKingdom/GameSprites.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoKingdom
+{
+    class GameSprites
+    {
+        public static ContentManager Content { get; set; }
+        public Texture2D Sprite { get; set; }
+
+        public GameSprites(ContentManager content, Texture2D sprite)
+        {
+            Content = content;            
+        }
+
+        public static Texture2D LoadSprite(string fileName)
+        {
+            return Content.Load<Texture2D>(fileName);
+        }
+    }
+}
